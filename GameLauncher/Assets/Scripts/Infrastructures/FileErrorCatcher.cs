1733c53971098a278de00257a872a96dd1291f44
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-12-19 15:02:23 +0900

    ファイルのエラーキャッチ用のクラスの作成

---
+++ GameLauncher/Assets/Scripts/Infrastructures/FileErrorCatcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+
+namespace GameLauncher.Infrastructures
+{
+    public static class FileErrorCatcher
+    {
+        public static string[] CatchError(Func<string, string[]> func, string argument)
+        {
+            string[] errors = new string[3];
+
+            try
+            {
+                return func(argument);
+            }
+            catch (FileNotFoundException e)
+            {
+                EditorUtility.DisplayDialog("❌Error", "ファイルパスに間違いがあります。\n" + e.Message, "OK");
+                errors[0] = e.Message;
+                return errors;
+            }
+            catch (ArgumentException e)
+            {
+                EditorUtility.DisplayDialog("❌Error", "ファイルパスが空欄です。\n" + e.Message, "OK");
+                errors[0] = e.Message;
+                return errors;
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                EditorUtility.DisplayDialog("❌Error", "ファイルパスに間違いがあります。\n" + e.Message, "OK");
+                errors[0] = e.Message;
+                return errors;
+
+            }
+        }
+    }
+}
