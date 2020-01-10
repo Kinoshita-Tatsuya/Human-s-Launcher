f2e0c4cef3f93e443123abbc0131b5074dc850a7
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2020-01-10 18:30:45 +0900

    入力できるかでハートの色を変更する

--- GameLauncher/Assets/Scripts/Components/HeartButton.cs
+++ GameLauncher/Assets/Scripts/Components/HeartButton.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace GameLauncher.Components
-{
+{    
     public class HeartButton : MonoBehaviour
     {
-        [SerializeField] private float reinputTime_s = 0.0f;
+        [SerializeField] float reinputTime_s = 0.0f;
+        [SerializeField] Color CantInputColor;       
+
+        private void Start()
+        {
+            Heart = gameObject.GetComponent<Image>();
+            CanInputColor = Heart.color;
+        }
 
         public void IncreaseHeartNum(string key)
         {
@@ -26,8 +33,12 @@ namespace GameLauncher.Components
         private void Update()
         {
             LapTime = Mathf.Max(LapTime - Time.deltaTime, 0.0f);
+
+            Heart.color = (CanInput) ? CanInputColor : CantInputColor;           
         }
 
+        Image Heart = null;
+        Color CanInputColor;
         private bool CanInput => LapTime <= 0;
         private float LapTime;
     }
